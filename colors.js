5dd6a4b7642eb054bdc119df400d68a5903047b7
choihuk <love72huk@naver.com>
2023-06-15 15:49:35 +0900

    feature: 켈린더 추가

--- colors.js
+++ colors.js
@@ -7,6 +7,14 @@ export const PRIMARY = {
   DARK: '#ff6781',
 };
 
+export const SECONDARY = {
+  LIGHT: '#ceffff',
+  DEFAULT: '#67ffff',
+  DARK: '#25a2a2',
+};
+
 export const GRAY = {
-  DARK: '#4f3d40',
+  LIGHT: '#dbd9d9',
+  DEFAULT: '#756e6f',
+  DARK: '#30282a',
 };
---
+++ components/WordList.js
@@ -0,0 +1,14 @@
+import { View, StyleSheet } from 'react-native';
+import { PRIMARY } from '../colors';
+
+const WordList = () => {
+  return <View style={styles.container}></View>;
+};
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // backgroundColor: PRIMARY.LIGHT,
+  },
+});
+
+export default WordList;
