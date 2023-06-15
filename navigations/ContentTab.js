92c1fa497fd8bc9a268e09dc5f73542c1897135d
woowal <jijile44@gmail.com>
2023-06-15 23:42:36 +0900

    퀴즈 네비게이션 분할

--- navigations/ContentTab.js
+++ navigations/ContentTab.js
@@ -7,6 +7,8 @@ import SettingScreen from '../screens/SettingScreen';
 import { ContentRoutes } from './routes';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { GRAY, PRIMARY } from '../colors';
+import WordQuizScreen from '../screens/WordQuizScreen';
+import QuizStack from './QuizStack';
 const Tab = createBottomTabNavigator();
 
 const getTabBarIcon = ({ focused, color, size, name }) => {
@@ -42,7 +44,7 @@ const ContentTab = () => {
       />
       <Tab.Screen
         name={ContentRoutes.Quiz.name}
-        component={QuizScreen}
+        component={QuizStack}
         options={{
           tabBarIcon: (props) => getTabBarIcon({ ...props, name: 'chat-question' }),
           tabBarLabel: ContentRoutes.Quiz.label,
