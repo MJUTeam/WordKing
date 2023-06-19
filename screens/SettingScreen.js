0e7b43cae2af37cddd5c42a8edc8b5d6ee3c55ad
wodnd0131 <wodnd0131@naver.com>
2023-06-19 22:42:52 +0900

    로그아웃버튼삭제

--- screens/SettingScreen.js
+++ screens/SettingScreen.js
@@ -1,17 +1,11 @@
 import { StyleSheet, View, Text } from 'react-native';
-import { firebaseAuth } from '../api/firebase';
 import Button from '../components/Button';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const signOut = () => {
-  firebaseAuth.signOut();
-};
 
 const SettingScreen = () => {
   return (
     <View style={styles.container}>
       <Text>SettingScreen</Text>
-      <Button title="Logout" onPress={signOut} />
       <Button title="Clear Data" onPress={ () => AsyncStorage.clear() } />
     </View>
   );
